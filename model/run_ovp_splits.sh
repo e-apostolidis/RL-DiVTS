6ecb91a6e25103f92e7e5eea85dfa48dd6535d7d
e-apostolidis <apostolid@iti.gr>
2023-02-24 11:00:31 +0200

    Delete run_ovp_splits.sh

--- model/run_ovp_splits.sh
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Bash script to automate the training of the model for the `OVP` dataset.
-# Runs main.py 5 times, for each data-split and for the selected number of epochs and batch size.
-
-for exp_id in $(seq 1 5); do
-  for split_id in $(seq 0 4); do
-    python model/main.py --split_index "$split_id" --n_epochs 150 --batch_size 40 --video_type 'OVP' --exp "$exp_id"
-  done
-done
